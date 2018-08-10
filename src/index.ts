6831573ae8af96fb652212ca1dff935dc54ca953
Simon Oulevay <simon.oulevay@gmail.com>
2018-08-10 11:26:46 +0200

    Spinners

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { createHash } from 'crypto';
 import * as glob from 'fast-glob';
 import { createReadStream, readFile, ReadStream, Stats, writeFile } from 'fs-extra';
 import { compact, isInteger, pick, uniq } from 'lodash';
+import * as ora from 'ora';
 
 import { program } from './program';
 
@@ -26,17 +27,33 @@ export interface GlobStats extends Stats {
 
 export async function dgstore(options: DgstoreOptions) {
 
-  const filesAndDigests = await glob<GlobStats>(options.targets, { stats: true });
+  const listSpinner = ora('Listing files').start();
 
-  const files = filesAndDigests.filter((stats: GlobStats) => !stats.path.match(/\.sha512$/) || !filesAndDigests.find(other => other.path === stats.path.replace(/\.sha512$/, '')));
+  let filesAndDigests: GlobStats[];
+  try {
+    filesAndDigests = await glob<GlobStats>(options.targets, { stats: true });
+  } catch (err) {
+    listSpinner.fail();
+    throw err;
+  }
+
+  const stats = filesAndDigests.filter((current: GlobStats) => !current.path.match(/\.sha512$/) || !filesAndDigests.find(other => other.path === current.path.replace(/\.sha512$/, '')));
+
+  listSpinner.succeed(`${stats.length} matching files found`);
 
   // TODO: parallelize
-  for (const file of files) {
-    await compareOrStoreDigest(file, options);
+  for (const stat of stats) {
+    const spinner = ora(`Hashing ${stat.path}`).start();
+    try {
+      await compareOrStoreDigest(stat, spinner, options);
+    } catch (err) {
+      spinner.fail();
+      throw err;
+    }
   }
 }
 
-async function compareOrStoreDigest(stats: GlobStats, options: DgstoreOptions) {
+async function compareOrStoreDigest(stats: GlobStats, spinner: any, options: DgstoreOptions) {
 
   const previousDigest = await readDigest(stats);
   const digest = await hash(stats.path);
@@ -44,18 +61,18 @@ async function compareOrStoreDigest(stats: GlobStats, options: DgstoreOptions) {
   const shortDigestOptions = pick(options, 'fullDigest');
 
   if (previousDigest && previousDigest.equals(digest)) {
-    process.stdout.write(` ${chalk.bold(chalk.green('ok'))} ${chalk.green(getShortDigest(digest, shortDigestOptions))} ${stats.path} ${chalk.gray('(no change)')}\n`);
+    spinner.succeed(`${chalk.green(getShortDigest(digest, shortDigestOptions))} ${stats.path} ${chalk.gray('(no change)')}`);
   } else if (previousDigest) {
-    process.stdout.write(`${chalk.bold(chalk.red('NOK'))} ${chalk.red(getShortDigest(digest, { ...shortDigestOptions, differentThan: previousDigest.toString('hex') }))} ${stats.path} ${chalk.yellow(`(previous digest was ${getShortDigest(previousDigest, { ...shortDigestOptions, differentThan: digest.toString('hex') })})`)}\n`);
+    spinner.warn(`${chalk.red(getShortDigest(digest, { ...shortDigestOptions, differentThan: previousDigest.toString('hex') }))} ${stats.path} ${chalk.yellow(`(previous digest was ${getShortDigest(previousDigest, { ...shortDigestOptions, differentThan: digest.toString('hex') })})`)}`);
   } else {
 
     let storedMessage = '';
     if (options.write) {
       await storeDigest(stats, digest);
-      storedMessage = ` ${chalk.yellow(`(stored digest to ${stats.path}.sha512)`)}`;
+      storedMessage = `${chalk.yellow(` (stored digest to ${stats.path}.sha512)`)}`;
     }
 
-    process.stdout.write(`${chalk.bold(chalk.cyan('new'))} ${chalk.cyan(getShortDigest(digest, shortDigestOptions))} ${stats.path}${storedMessage}\n`);
+    spinner.succeed(`${chalk.cyan(getShortDigest(digest, shortDigestOptions))} ${stats.path}${storedMessage}`);
   }
 }
 
